2ce6f6981af450bba9e98e69f02f26e17aed9782
Jarryd-pretorius <86589746+Jarryd-pretorius@users.noreply.github.com>
2021-07-28 10:56:14 +0800

    basic working app

--- index.js
+++ index.js
@@ -11,23 +11,28 @@ function newUrl() {
 
   
 async function getPlanetWeather() {
+    
     const info_url = newUrl();
     const planet_response = await fetch(info_url);
     const planet_data = await planet_response.json();
     const planet_name = planet_data.englishName
-
-    document.getElementById('merc').innerHTML = planet_name;
-    document.getElementById('title').innerHTML = planet_name;
     const TempK = planet_data.avgTemp;
     const TempC = TempK -273.15;
     const TempC_rounded = TempC.toFixed(2);
+    const Mass = planet_data.mass.massValue;
+    const MassRounded = Mass.toFixed(1)+"^"+planet_data.mass.massExponent;
+    const orbit = planet_data.sideralOrbit;
+    const orbitYears = (orbit/365).toFixed(2);
+
+    document.getElementById('merc').innerHTML = planet_name;
+    document.getElementById('title').innerHTML = planet_name;
     document.getElementById('temp').innerHTML = TempC_rounded;
     document.getElementById('clos_sun').innerHTML = 
     planet_data.perihelion;
     document.getElementById('far_sun').innerHTML =
     planet_data.aphelion;
     document.getElementById('mass').innerHTML = 
-    planet_data.massValue;
+    MassRounded;
     document.getElementById('grav').innerHTML = 
     planet_data.gravity;
     document.getElementById('radi').innerHTML = 
@@ -35,7 +40,6 @@ async function getPlanetWeather() {
     document.getElementById('esc_vel').innerHTML = 
     planet_data.escape;
     const numMoons = planet_data.moons.length;
-    console.log(numMoons);
     document.getElementById('moons').innerHTML = 
     numMoons;
     document.getElementById('french').innerHTML = 
@@ -47,9 +51,10 @@ async function getPlanetWeather() {
     document.getElementById('day').innerHTML = 
     planet_data.sideralRotation;
     document.getElementById('orbit').innerHTML = 
-    planet_data.sideralOrbit;
-
+    orbitYears;
 }
+
+ 
 
 function changeTempToF() {
     let temp = document.getElementById('temp').innerText;
