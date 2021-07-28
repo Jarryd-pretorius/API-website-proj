43e964df57339294bd2d8021bf80cc8b1cfbf6a0
Jarryd-pretorius <86589746+Jarryd-pretorius@users.noreply.github.com>
2021-07-28 10:01:37 +0800

    Javascript additions

---
+++ index.js
@@ -0,0 +1,70 @@
+const Mars_url = 'https://mars.nasa.gov/rss/api/?feed=weather&category=msl&feedtype=json'
+let Planet_API = 'https://api.le-systeme-solaire.net/rest/bodies/'
+
+
+ 
+function newUrl() {
+   let data = document.getElementById('query').value;
+   console.log(data);
+   return Planet_API + data; 
+}
+
+  
+async function getPlanetWeather() {
+    const info_url = newUrl();
+    const planet_response = await fetch(info_url);
+    const planet_data = await planet_response.json();
+    const planet_name = planet_data.englishName
+
+    document.getElementById('merc').innerHTML = planet_name;
+    document.getElementById('title').innerHTML = planet_name;
+    const TempK = planet_data.avgTemp;
+    const TempC = TempK -273.15;
+    const TempC_rounded = TempC.toFixed(2);
+    document.getElementById('temp').innerHTML = TempC_rounded;
+    document.getElementById('clos_sun').innerHTML = 
+    planet_data.perihelion;
+    document.getElementById('far_sun').innerHTML =
+    planet_data.aphelion;
+    document.getElementById('mass').innerHTML = 
+    planet_data.massValue;
+    document.getElementById('grav').innerHTML = 
+    planet_data.gravity;
+    document.getElementById('radi').innerHTML = 
+    planet_data.meanRadius;
+    document.getElementById('esc_vel').innerHTML = 
+    planet_data.escape;
+    const numMoons = planet_data.moons.length;
+    console.log(numMoons);
+    document.getElementById('moons').innerHTML = 
+    numMoons;
+    document.getElementById('french').innerHTML = 
+    planet_data.name;
+    document.getElementById('dis_by').innerHTML = 
+    planet_data.discoveredBy;
+    document.getElementById('date').innerHTML = 
+    planet_data.discoveryDate;
+    document.getElementById('day').innerHTML = 
+    planet_data.sideralRotation;
+    document.getElementById('orbit').innerHTML = 
+    planet_data.sideralOrbit;
+
+}
+
+function changeTempToF() {
+    let temp = document.getElementById('temp').innerText;
+    convertedTemp = (temp*2) + 30;
+        document.getElementById('temp').innerHTML = convertedTemp;
+   
+}
+
+function changeTempToC () {
+    let temp = document.getElementById('temp').innerText;
+    convertedTemp = (temp-30) / 2;
+        document.getElementById('temp').innerHTML = convertedTemp;
+}
+
+
+
+
+
